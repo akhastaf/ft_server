f7daa996a4776ce806767a612e7f8d2ff5fd319e
Abderrazzaq Khastaf <akhastaf@e1r13p5.1337.ma>
2019-11-24 20:45:12 +0100

    secure the root user for mysql

--- srcs/start.sh
+++ srcs/start.sh
@@ -5,5 +5,6 @@ mysql -uroot <<<"CREATE DATABASE IF NOT EXISTS test ;"
 mysql -uroot <<<"CREATE USER 'wp'@'%' IDENTIFIED BY '123456' ;"
 mysql -uroot <<<"GRANT ALL ON test.* TO 'wp'@'%' ;"
 mysql -uroot <<<"FLUSH PRIVILEGES ;"
+mysql -uroot <<<"ALTER USER 'root'@'localhost' IDENTIFIED BY 'root' ;"
 nginx -g "daemon off;"
 /bin/bash
